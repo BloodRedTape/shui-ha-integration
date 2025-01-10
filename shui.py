ded1e78648c706a4d59c5a97619bf1b344a4a8c1
I.Kostiuk <gefestroyale@gmail.com>
2025-01-10 18:21:01 +0000

    Factor out connection logic from state logic

--- shui.py
+++ shui.py
@@ -8,53 +8,22 @@ class GCode:
     BEEP_SOUND = "M300"
 
 
-class Shui3dPrinterConnectionStatus(Enum):
-    Disconnected = 0
-    Connected = 1
-
-
-class Shui3dPrintStatus(Enum):
-    Idle = 0
-    Busy = 1
-    Printing = 2
-
-
-def StdLogger(message: str):
-    print(message)
-
-
-class Shui3dPrinter:
+class Shui3dPrinterConnection:
     CONNECTION_TIMEOUT = 2
     READ_TIMEOUT = 4
     DELAY_AFTER_CMD_WRITE = 4
-    FAILS_TO_DISCONNECT = 3
 
     _ip: str
     _port: int
     _reader: asyncio.StreamReader
     _writer: asyncio.StreamWriter
 
-    _bed_temp: float = 0
-    _target_bed_temp: float = 0
+    class CanNotConnect:
+        pass
 
-    _extruder_temp: float = 0
-    _target_extruder_temp: float = 0
-
-    _print_progress: float = 0
-    _print_status: Shui3dPrintStatus
-
-    _status: Shui3dPrinterConnectionStatus = Shui3dPrinterConnectionStatus.Disconnected
-
-    _update: bool = False
-
-    _logger: Callable[[str], None]
-
-    _disconnected: int = 0
-
-    def __init__(self, ip: str, port: int, logger: Callable[[str], None] = StdLogger):
+    def __init__(self, ip: str, port: int):
         self._ip = ip
         self._port = port
-        self._logger = logger
 
     async def read_all(self, chunk_size: int = 10000):
         data = await self._reader.read(chunk_size)
@@ -74,72 +43,105 @@ class Shui3dPrinter:
 
         return lines
 
-    async def exec(self, snippet: str) -> List[str]:
+    async def exec(self, snippet: str) -> List[str] | CanNotConnect:
         try:
             (self._reader, self._writer) = await asyncio.wait_for(
                 asyncio.open_connection(self._ip, self._port),
-                Shui3dPrinter.CONNECTION_TIMEOUT,
+                Shui3dPrinterConnection.CONNECTION_TIMEOUT,
             )
         except Exception:
-            return []
+            return Shui3dPrinterConnection.CanNotConnect()
 
         lines: List[str] = []
 
         try:
             lines = await asyncio.wait_for(
-                self.read_lines(4), Shui3dPrinter.READ_TIMEOUT
+                self.read_lines(4), Shui3dPrinterConnection.READ_TIMEOUT
             )
         except Exception as e:
-            self.log(f"Intro read failed with {e}")
-            return [type(e).__name__]
+            return lines + [type(e).__name__]
 
         try:
             self._writer.write((snippet + "\n\r").encode())
             await self._writer.drain()
         except Exception as e:
-            self.log(f"Write failed with: {type(e).__name__}")
-            return [type(e).__name__]
+            return lines + [type(e).__name__]
 
-        await asyncio.sleep(Shui3dPrinter.DELAY_AFTER_CMD_WRITE)
+        await asyncio.sleep(Shui3dPrinterConnection.DELAY_AFTER_CMD_WRITE)
 
         try:
-            buffer = await asyncio.wait_for(self.read_all(), Shui3dPrinter.READ_TIMEOUT)
+            buffer = await asyncio.wait_for(
+                self.read_all(), Shui3dPrinterConnection.READ_TIMEOUT
+            )
 
             lines = lines + buffer.split("\n")
-        except asyncio.TimeoutError:
-            return ["asyncio.TimeoutError"]
         except Exception as e:
-            self.log(f"Read failed with: {e}")
-            return [type(e).__name__]
+            return lines + [type(e).__name__]
 
         await self._writer.drain()
         self._writer.close()
         await self._writer.wait_closed()
         return lines
 
+
+class Shui3dPrinterConnectionStatus(Enum):
+    Disconnected = 0
+    Connected = 1
+
+
+class Shui3dPrintStatus(Enum):
+    Idle = 0
+    Busy = 1
+    Printing = 2
+
+
+def StdLogger(message: str):
+    print(message)
+
+
+class Shui3dPrinter:
+    FAILS_TO_DISCONNECT = 3
+
+    _bed_temp: float = 0
+    _target_bed_temp: float = 0
+
+    _extruder_temp: float = 0
+    _target_extruder_temp: float = 0
+
+    _print_progress: float = 0
+    _print_status: Shui3dPrintStatus
+
+    _status: Shui3dPrinterConnectionStatus = Shui3dPrinterConnectionStatus.Disconnected
+
+    _update: bool = False
+    _update_connection: Shui3dPrinterConnection
+
+    _logger: Callable[[str], None]
+
+    _disconnected: int = 0
+
+    def __init__(self, ip: str, port: int, logger: Callable[[str], None] = StdLogger):
+        self._ip = ip
+        self._port = port
+        self._logger = logger
+        self._update_connection = Shui3dPrinterConnection(ip, port)
+
+    def log(self, message: str):
+        self._logger(message)
+
     async def beep(self):
-        await self.exec(GCode.BEEP_SOUND)
+        await Shui3dPrinterConnection(self._ip, self._port).exec(GCode.BEEP_SOUND)
 
     async def update(self):
-        lines = await self.exec(GCode.SD_PRINT_STATUS)
+        lines = await self._update_connection.exec(GCode.SD_PRINT_STATUS)
 
-        for line in lines:
-            # self.log("Line: " + line)
-            try:
-                self.update_values_based_on(line)
-            except Exception as e:
-                self.log(f"Update failed with: {e}")
+        if isinstance(lines, Shui3dPrinterConnection.CanNotConnect):
+            self._disconnected += 1
+        else:
+            self.update_from(lines)
 
-        try:
-            self.update_print_status(lines)
-        except Exception as e:
-            self.log(f"Progress update failed with: {type(e).__name__}")
-
-        if len(lines):
             self._disconnected = 0
             self._status = Shui3dPrinterConnectionStatus.Connected
-        else:
-            self._disconnected += 1
 
         if self._disconnected >= Shui3dPrinter.FAILS_TO_DISCONNECT:
             self._status = Shui3dPrinterConnectionStatus.Disconnected
@@ -157,7 +159,23 @@ class Shui3dPrinter:
 
         self._update = False
 
-    def update_values_based_on(self, line: str):
+    def update_from(self, lines: List[str]):
+        self.update_values_from(lines)
+
+        try:
+            self.update_statues_from(lines)
+        except Exception as e:
+            self.log(f"Progress update failed with: {type(e).__name__}")
+
+    def update_values_from(self, lines: List[str]):
+        for line in lines:
+            # self.log("Line: " + line)
+            try:
+                self.parse_and_update_values(line)
+            except Exception as e:
+                self.log(f"Update failed with: {e}")
+
+    def parse_and_update_values(self, line: str):
         parts: list[str] = line.split()
 
         for i in range(len(parts)):
@@ -169,7 +187,7 @@ class Shui3dPrinter:
                 self._bed_temp = float(part.split(":")[1])
                 self._target_bed_temp = float(parts[i + 1].split("/")[-1])
 
-    def update_print_status(self, lines: List[str]):
+    def update_statues_from(self, lines: List[str]):
         for line in lines:
             if "SD printing byte" in line:
                 nm = line.split()[-1].split("/")
@@ -181,9 +199,6 @@ class Shui3dPrinter:
                 break
             else:
                 self._print_status = Shui3dPrintStatus.Idle
-
-    def log(self, message: str):
-        self._logger(message)
 
     def status(self):
         return str(self._status).split(".")[1]
